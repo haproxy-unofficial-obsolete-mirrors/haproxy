haproxy-unofficial-obsolete-mirrors/haproxy

commit f54f8bdd8d2a40417e0fa03bdf8dfa5a4de73cfc

[MINOR] maintain a global session list in order to ease debugging
Now the global variable 'sessions' will be a dual-linked list of all
known sessions. The list element is set at the beginning of the session
so that it's easier to follow them all with gdb.

src/client.c
@@ -53,7 +53,7 @@ void get_frt_addr(struct session *s)
 /*
  * FIXME: This should move to the STREAM_SOCK code then split into TCP and HTTP.
  */
-    
+
 /*
  * this function is called on a read event from a listen socket, corresponding
  * to an accept. It tries to accept as many connections as possible.
@@ -105,6 +105,8 @@ int event_accept(int fd) {
 			p->state = PR_STIDLE;
 			goto out_close;
 		}
+
+		LIST_ADDQ(&sessions, &s->list);
 
 		s->flags = 0;
 		s->term_trace = 0;
@@ -455,6 +457,7 @@ int event_accept(int fd) {
  out_free_task:
 	pool_free2(pool2_task, t);
  out_free_session:
+	LIST_DEL(&s->list);
 	pool_free2(pool2_session, s);
  out_close:
 	close(cfd);

include/proto/session.h
@@ -3,7 +3,7 @@
   This file defines everything related to sessions.
 
   Copyright (C) 2000-2008 Willy Tarreau - w@1wt.eu
-  
+
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation, version 2.1
@@ -27,6 +27,7 @@
 #include <types/session.h>
 
 extern struct pool_head *pool2_session;
+extern struct list sessions;
 
 void session_free(struct session *s);
 

include/types/session.h
@@ -3,7 +3,7 @@
   This file defines everything related to sessions.
 
   Copyright (C) 2000-2008 Willy Tarreau - w@1wt.eu
-  
+
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation, version 2.1
@@ -152,6 +152,7 @@ enum {
  *    server should eventually be released.
  */
 struct session {
+	struct list list;			/* position in global sessions list */
 	struct task *task;			/* the task associated with this session */
 	/* application specific below */
 	struct proxy *fe;			/* the proxy this session depends on for the client side */

src/proto_uxst.c
@@ -411,12 +411,15 @@ int uxst_event_accept(int fd) {
 			return 0;
 		}
 
+		LIST_ADDQ(&sessions, &s->list);
+
 		s->flags = 0;
 		s->term_trace = 0;
 
 		if ((t = pool_alloc2(pool2_task)) == NULL) {
 			Alert("out of memory in uxst_event_accept().\n");
 			close(cfd);
+			LIST_DEL(&s->list);
 			pool_free2(pool2_session, s);
 			return 0;
 		}
@@ -428,6 +431,7 @@ int uxst_event_accept(int fd) {
 			Alert("accept(): not enough free sockets. Raise -n argument. Giving up.\n");
 			close(cfd);
 			pool_free2(pool2_task, t);
+			LIST_DEL(&s->list);
 			pool_free2(pool2_session, s);
 			return 0;
 		}
@@ -436,6 +440,7 @@ int uxst_event_accept(int fd) {
 			Alert("accept(): cannot set the socket in non blocking mode. Giving up\n");
 			close(cfd);
 			pool_free2(pool2_task, t);
+			LIST_DEL(&s->list);
 			pool_free2(pool2_session, s);
 			return 0;
 		}
@@ -467,6 +472,7 @@ int uxst_event_accept(int fd) {
 		if ((s->req = pool_alloc2(pool2_buffer)) == NULL) { /* no memory */
 			close(cfd); /* nothing can be done for this fd without memory */
 			pool_free2(pool2_task, t);
+			LIST_DEL(&s->list);
 			pool_free2(pool2_session, s);
 			return 0;
 		}
@@ -475,6 +481,7 @@ int uxst_event_accept(int fd) {
 			pool_free2(pool2_buffer, s->req);
 			close(cfd); /* nothing can be done for this fd without memory */
 			pool_free2(pool2_task, t);
+			LIST_DEL(&s->list);
 			pool_free2(pool2_session, s);
 			return 0;
 		}
@@ -577,7 +584,7 @@ static int process_uxst_cli(struct session *t)
 			buffer_shutr(req);
 			t->cli_state = CL_STSHUTR;
 			return 1;
-		}	
+		}
 		/* last server read and buffer empty */
 		else if ((s == SV_STSHUTR || s == SV_STCLOSE) && (rep->flags & BF_EMPTY)) {
 			EV_FD_CLR(t->cli_fd, DIR_WR);
@@ -607,7 +614,7 @@ static int process_uxst_cli(struct session *t)
 					t->flags |= SN_FINST_D;
 			}
 			return 1;
-		}	
+		}
 		/* write timeout */
 		else if (tick_is_expired(rep->wex, now_ms)) {
 			EV_FD_CLR(t->cli_fd, DIR_WR);

src/session.c
@@ -26,6 +26,7 @@
 
 
 struct pool_head *pool2_session;
+struct list sessions;
 
 /*
  * frees  the context associated to a session. It must have been removed first.
@@ -74,6 +75,7 @@ void session_free(struct session *s)
 	pool_free2(pool2_requri, txn->uri);
 	pool_free2(pool2_capture, txn->cli_cookie);
 	pool_free2(pool2_capture, txn->srv_cookie);
+	LIST_DEL(&s->list);
 	pool_free2(pool2_session, s);
 
 	/* We may want to free the maximum amount of pools if the proxy is stopping */
@@ -92,6 +94,7 @@ void session_free(struct session *s)
 /* perform minimal intializations, report 0 in case of error, 1 if OK. */
 int init_session()
 {
+	LIST_INIT(&sessions);
 	pool2_session = create_pool("session", sizeof(struct session), MEM_F_SHARED);
 	return pool2_session != NULL;
 }

commit 71f84ded5197c94afac6bebe03cb4b37241e154e

[BUG] client: don't add a new session to the list too early
During 1.3.16-dev, commit f54f8bdd8d2a40417e0fa03bdf8dfa5a4de73cfc
introduced a regression by creating a list of active sessions,
because a newly created session which is added to the list is
not removed if the request matches a monitor-net rule in TCP
mode. The result is that a dirty entry remains in the list,
later causing random crashes.

The fix consists in adding the session to the list only after
the monitor-net rules are checked. It must be backported to 1.3
and ported to 1.5-dev.

Thanks to Morten Gade Sørensen for reporting this bug with very
detailed diagnostics, and to Hervé Commowick for helping to find
a reproducer and isolating the affected versions. They also
confirmed the fix works.

src/client.c
@@ -126,7 +126,6 @@ int event_accept(int fd) {
 			goto out_close;
 		}
 
-		LIST_ADDQ(&sessions, &s->list);
 		LIST_INIT(&s->back_refs);
 
 		s->flags = 0;
@@ -145,6 +144,8 @@ int event_accept(int fd) {
 			}
 			s->flags |= SN_MONITOR;
 		}
+
+		LIST_ADDQ(&sessions, &s->list);
 
 		if ((t = task_new()) == NULL) { /* disable this proxy for a while */
 			Alert("out of memory in event_accept().\n");