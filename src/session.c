haproxy-unofficial-obsolete-mirrors/haproxy

commit d6e9e3b5e320b957e6c491bd92d91afad30ba638

[BUG] stick_table: fix possible memory leak in case of connection error
If a "stick store-request" rule is present, an entry is preallocated during
the request. However, if there is no response due to an error or to a redir
mode server, we never release it. This bug was found in 1.5-dev and is already
fixed there.

src/session.c
@@ -50,6 +50,7 @@ void session_free(struct session *s)
 	struct http_txn *txn = &s->txn;
 	struct proxy *fe = s->fe;
 	struct bref *bref, *back;
+	int i;
 
 	if (s->pend_pos)
 		pendconn_free(s->pend_pos);
@@ -81,6 +82,13 @@ void session_free(struct session *s)
 	pool_free2(pool2_buffer, s->rep);
 
 	http_end_txn(s);
+
+	for (i = 0; i < s->store_count; i++) {
+		if (!s->store[i].ts)
+			continue;
+		stksess_free(s->store[i].table, s->store[i].ts);
+		s->store[i].ts = NULL;
+	}
 
 	if (fe) {
 		pool_free2(fe->hdr_idx_pool, txn->hdr_idx.v);

commit 61ba936e6858dfcf9964d25870726621d8188fb9

[BUG] stick_table: the fix for the memory leak caused a regression
Commit d6e9e3b5e320b957e6c491bd92d91afad30ba638 caused recently created
entries to be removed as soon as they were created, breaking stickiness.
It is not clear whether a use-after-free was possible or not in this case.

This fix needs to be ported to 1.5-dev and is not needed in 1.3.

This bug was reported by Ben Congleton and narrowed down by Hervé Commowick,
both of whom also tested the fix. Thanks to them !

src/session.c
@@ -802,11 +802,12 @@ int process_store_rules(struct session *s, struct buffer *rep, int an_bit)
 
 	/* process store request and store response */
 	for (i = 0; i < s->store_count; i++) {
-		if (stktable_store(s->store[i].table, s->store[i].ts, s->srv->puid) > 0) {
+		if (stktable_store(s->store[i].table, s->store[i].ts, s->srv->puid) > 0)
 			stksess_free(s->store[i].table, s->store[i].ts);
-			s->store[i].ts = NULL;
-		}
+		/* always remove pointer to session to ensure we won't free it again */
+		s->store[i].ts = NULL;
 	}
+	s->store_count = 0; /* everything is stored */
 
 	rep->analysers &= ~an_bit;
 	rep->analyse_exp = TICK_ETERNITY;